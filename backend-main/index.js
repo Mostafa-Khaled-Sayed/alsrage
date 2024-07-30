23edeb351c99bdac5997d2aeb71bf1e2a7a6b7c4
mostafa12388888 <kostafa606888@gmail.com>
2024-07-30 22:14:46 +0300

    sfdsdfds

--- backend-main/index.js
+++ backend-main/index.js
@@ -68,9 +68,9 @@ app.use(express.json());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.post("/upload", upload.single("file"), (req, res) => {
   if (req.fileValidationError) {
